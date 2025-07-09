6feae85ecea4c821ee5c499ec665da2893020866
1308534183 <33587675+1308534183@users.noreply.github.com>
2025-07-09 18:01:15 +0800

    Update Alert.kt

--- android/app/src/main/kotlin/com/hiddify/hiddify/constant/Alert.kt
+++ android/app/src/main/kotlin/com/hiddify/hiddify/constant/Alert.kt
@@ -3,6 +3,7 @@ package com.hiddify.hiddify.constant
 enum class Alert {
     RequestVPNPermission,
     RequestNotificationPermission,
+    RequestStoragePermission,
     EmptyConfiguration,
     StartCommandServer,
     CreateService,
